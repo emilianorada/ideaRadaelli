fbb96ec6ece9e05d22192de13ea1fdb12ce5b748
Emiliano Radaelli <emiliano.rada7@gmail.com>
2022-03-16 22:57:05 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+//importo los componentes creados 
+import About from './layouts/about';
+import Contact from './layouts/contact';
+import Home from './layouts/home';
+import NavBar1 from './Components/NavBar';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          GHOSTNET TELECOMUNICACIONES
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={ <NavBar1 /> }  >
+                <Route index element={ <Home /> } />
+                <Route path='about' element={ <About /> } />
+                <Route path='contact' element={ <Contact /> } />
+
+                <Route path='*' element={ <Navigate replace to="/" /> } />
+            </Route>
+          </Routes>
+        
+        </BrowserRouter>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap"; 
+import { Outlet, Link } from "react-router-dom";
+
+
+const NavBar1 = () => {
+    return (
+    <>
+    <Navbar className="navBg"  expand="lg">
+  <Container>
+  <Navbar.Brand href="#home">
+            <img
+            alt=""
+            src="/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            />{' '}
+        </Navbar.Brand>
+
+    <Navbar.Brand as={Link} to="/" >React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/about">About</Nav.Link>
+                <Nav.Link as={Link} to="/contact" >Contact</Nav.Link>
+        </Nav>
+    </Navbar.Collapse>
+  </Container>
+    </Navbar>
+    <section>
+        <outlet>
+
+        </outlet>
+    </section>
+
+
+    </>
+        
+
+    
+    )
+};
+
+
+
+
+export default NavBar1
