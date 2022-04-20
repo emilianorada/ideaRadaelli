e0c0924531d4e64b7b940328cf4c897bbcd692f0
Emiliano Radaelli <emiliano.rada7@gmail.com>
2022-04-20 13:47:13 -0300

    Filtrado por categorías (En tienda)

--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,23 +1,41 @@
 import React, { useEffect, useState } from 'react'
-import { getProducts } from '../../FakeApi'
+import {Container} from "react-bootstrap"
+import { useParams} from "react-router-dom"
+import { pedirDatos } from '../../helpers/pedirDatos'
 import ItemDetail from '../ItemDetail/ItemDetail'
 
 const ItemDetailContainer = () => {
-const [productDetail, setProductDetail] = useState({})
-const [CargaDetalles, setCargaDetalles] = useState(false)
 
+  const [productDetail, setProductDetail] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const {itemId} = useParams()
+  
+  
         useEffect(()=>{
-            setCargaDetalles(true)
-            getProducts
-            .then((res) => setProductDetail(res.find((item) => item.id === '05')))
-            .catch((error) => console.log(error))
-            .finally(() => setCargaDetalles(false))
-        }, [])
+            setLoading(true)
+            
+            pedirDatos()
+            .then((res) => {
+                  setProductDetail(res.find((item) => item.id === Number(itemId)))
+            })
+                  .catch((error) => {
+                    console.log(error)
+                  })
+            
+                  .finally(() => {
+                    setLoading(false)
+        })
+      }, [itemId])
+
   return (
-    <div className='row'>
-        {CargaDetalles ? <p className='p-3'>CARGANDO DETALLES</p> :  <ItemDetail productDetail={productDetail}/>}
-       
-    </div>
+    <Container className='row my-5'>
+        {
+          loading 
+          ? <p className='p-3'>CARGANDO DETALLES</p> 
+          :  <ItemDetail {...productDetail}  />
+        }       
+    </Container>
   )
 }
 
