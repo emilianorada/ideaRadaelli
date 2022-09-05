5fe38720242e947c9e80223523e94ab38727becc
Emiliano Radaelli <emiliano.rada7@gmail.com>
2022-09-05 10:18:05 -0300

    Agregado de ItemCount

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import React, { useState } from 'react';
 import './App.css';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import NavBar from './Components/NavBar/NavBar';
 import "./styles/NavBar.css";
-import "./styles/ItemListContainer.css"
+import "./styles/ItemListContainer.css";
+import "./styles/ItemCount.css";
+import ItemCount from './Components/ItemCount/ItemCount';
+
+
 
 function App() {
+
+  const stock = 10
+  const [items, setItems] = useState(0)
+  const sumar = () => items < stock ? setItems(items + 1) : alert('Máximo de items alcanzado.')
+  const restar = () => items > 0 ? setItems(items - 1) : alert('No pueden introducirse valores negativos')
+
+
+
   return (
     
     <div>
       <NavBar />
       <ItemListContainer titulo='Tienda' />
+      <ItemCount stock={stock} sumar={sumar} restar={restar} items={items} />
     </div>
   );
 }
---
+++ src/Components/ItemCount/ItemCount.js
@@ -0,0 +1,20 @@
+import "../../styles/ItemCount.css";
+
+
+const ItemCount = ({items, stock, sumar, restar}) => {
+
+    return (
+        
+        <div className="itemCount">
+            <div className="numberSelected">{items}</div>
+            <div className="count">
+                <button className="buttonQuantity" onClick={restar}>-</button>
+                <button className="buttonQuantity" onClick={sumar}>+</button>
+            </div>
+            <button className="buttonAdd" >Agregar al carrito</button>    
+        <div className="detail">Stock disponible: {stock}</div>   
+        </div>
+    )
+}
+
+export default ItemCount
