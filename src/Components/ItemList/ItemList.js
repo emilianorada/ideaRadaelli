6933709be66cde0f2e845cc3d73e5fd8e91fd24c
Emiliano Radaelli <emiliano.rada7@gmail.com>
2022-09-11 19:28:39 -0300

    Item + ItemList

---
+++ src/Components/ItemList/ItemList.js
@@ -0,0 +1,23 @@
+import Item from '../Item/Item';
+
+
+const ItemList = ({ lista }) => {
+
+
+    return(
+        <div>
+            {lista.map((product) => (
+                <div key={product.id}>
+                    <Item 
+                        title={product.title}
+                        price={product.price}
+                        image= {product.image}
+                    />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+
+export default ItemList;
